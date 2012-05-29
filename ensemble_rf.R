a3807773d1f19f19b6467c3a1da3b89ed234363c
rockclimber112358 <rockclimber112358@gmail.com>
2012-05-29 06:09:09 -0600

    Added in regression functionality.

--- ensemble_rf.R
+++ ensemble_rf.R
@@ -1,7 +1,8 @@
 rf.ensem = function( d, test.bt, cv.group, ind_col, dep_cols
       ,tree.vec = 2^(1:13)
       #Controls how many variables are sampled for each split:
-      ,try.vec = sqrt( ncol( d ) )
+      ,try.vec = floor( sqrt( ncol( d ) ) )
+      ,type	= "Classification"	# "Regression"
       ,name = ""
       )
 {
@@ -19,15 +20,30 @@ rf.ensem = function( d, test.bt, cv.group, ind_col, dep_cols
     {
       for( i in 1:num.groups )
       {
-        t = randomForest( x=d[cv.group != i & cv.group>0,dep_cols]
+        if( type == "Classification" )
+        {
+          t = randomForest( x=d[cv.group != i & cv.group>0,dep_cols]
             , y = as.factor(d[cv.group != i & cv.group>0, ind_col])
             , n.trees = k
             , mtry = j )
-        ensem.rf[cv.group==i,col.csr] =
+          ensem.rf[cv.group==i,col.csr] =
             predict( t, newdata=d[cv.group==i,dep_cols], type="prob" )[,2]
-        ensem.rf[test.bt==1,col.csr] =
+          ensem.rf[test.bt==1,col.csr] =
             ensem.rf[test.bt==1,col.csr] +
             predict( t, newdata=d[test.bt==1,dep_cols], type="prob" )[,2]*1/num.groups
+        }
+        if( type == "Regression" )
+        {
+          t = randomForest( x=d[cv.group != i & cv.group>0,dep_cols]
+            , y = d[cv.group != i & cv.group>0, ind_col]
+            , n.trees = k
+            , mtry = j )
+          ensem.rf[cv.group==i,col.csr] =
+            predict( t, newdata=d[cv.group==i,dep_cols], type="response" )
+          ensem.rf[test.bt==1,col.csr] =
+            ensem.rf[test.bt==1,col.csr] +
+            predict( t, newdata=d[test.bt==1,dep_cols], type="response" )*1/num.groups
+        }
         colnames( ensem.rf )[col.csr] = paste( "rf_trees", k, "_try",round(j,0),sep="")
         if( name!="") colnames(ensem.rf)[col.csr] = paste(colnames(ensem.rf)[col.csr],"_",name,sep="")
         print( paste( "This analysis is ", round(100*(i+(which(tree.vec==k)-1)*num.groups+num.groups*length(tree.vec)*(which(try.vec==j)-1))/(length(try.vec)*length(tree.vec)*num.groups)),"% complete",sep="" ) )
@@ -41,9 +57,10 @@ rf.ensem = function( d, test.bt, cv.group, ind_col, dep_cols
 }
 
 rf.ensem.pc = function( d, test.bt, cv.group, ind_col, dep_cols
-      ,tree.vec = 2^(1:13)
+      ,tree.vec	= 2^(1:13)
       #Controls how many variables are sampled for each split:
-      ,try.vec = sqrt( ncol( d ) )
+      ,try.vec	= sqrt( length( dep_cols ) )
+	,type		= "Classification"
       ,pc.vec
       )
 {
@@ -51,7 +68,7 @@ rf.ensem.pc = function( d, test.bt, cv.group, ind_col, dep_cols
   output = matrix(0,nrow=nrow(d), ncol=0 )
   for( i in pc.vec )
   {
-    output.temp = rf.ensem( p, test.bt, cv.group, 1, 2:(i+1), tree.vec, try.vec, name=paste("pc",i,sep="") )
+    output.temp = rf.ensem( p, test.bt, cv.group, 1, 2:(i+1), tree.vec, try.vec, name=paste("pc",i,sep=""), type=type )
     output = cbind(output,output.temp)
   }
   return( output )

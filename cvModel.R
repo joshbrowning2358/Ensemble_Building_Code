7b1b807836654316653a2bc31ce912299a0cc6ae
rockclimber112358 <rockclimber112358@gmail.com>
2014-06-23 09:38:28 -0600

    Big Update!
    Revamped how cvModel works by allowing the passing of a fitting function and a prediction function.  cvModel is now highly flexible and can work with most models.  See the "Sample cvModels.R" file for an example of how to implement most of the common model types.

--- cvModel.R
+++ cvModel.R
@@ -32,50 +32,6 @@ fit.AMORE = function( form, data, hidden, steps=1000, print=10, learning.rate.gl
   return(list(hidden.wts=hidden.wts, output.wts=output.wts, activation.function=hidden.layer, form=form))
 }
 
-fit.glmnet = function(form, data, lambda=1*(0.9)^(0:100), family=c("gaussian","binomial","poisson","multinomial","cox","mgaussian")
-                      ,alpha = 1, nlambda = 100, maxit=100000 ){
-  #Parse the form object to determine number of input neurons and relevant data
-  if( !is.formula(form) ) stop("Formula incorrectly specified")
-  form = as.character( form )
-  indCol = (1:ncol(data))[colnames(data)==form[2]]
-  depVars = strsplit(form[3],"+", fixed=T)
-  #period matches everything:
-  if( depVars=="." ){
-    depCols = (1:ncol(data))[-indCol]
-  } else {
-    depVars = sapply( depVars, function(x)gsub(" ","",x) )
-    depCols = (1:ncol(data))[colnames(data) %in% depVars]
-  }
-  
-  filter = apply( data[,c(depCols,indCol)], 1, function(x){all(!is.na(x))} )
-  if(any(!filter)){
-    warning("Missing values in data frame. Removed for analysis")
-    data = data[filter,]
-  }
-  
-  glmnet(x=as.matrix(data[,depCols]), y=as.matrix(data[,indCol]), lambda=lambda, family=family, alpha=alpha, nlambda=nlambda, maxit=maxit)
-}
-
-fit.rbf = function( form, data, size, maxit=1000, linOut=T, print=10, report=T ){
-  #Parse the form object to determine number of input neurons and relevant data
-  if( !is(form, "formula") ) stop("Formula incorrectly specified")
-
-  form = as.character( form )
-  indCol = (1:ncol(data))[colnames(data)==form[2]]
-  depVars = strsplit(form[3],"+", fixed=T)
-  #period matches everything:
-  if( depVars=="." ){
-    depCols = (1:ncol(data))[-indCol]
-  } else {
-    depVars = sapply( depVars, function(x)gsub(" ","",x) )
-    depCols = (1:ncol(data))[colnames(data) %in% depVars]
-  }
-  
-  #Fit the neural network
-  mod = rbf(data[,depCols], data[,indCol], size=size, maxit=maxit, linOut=linOut)
-  return(mod)
-}
-
 #mod: A list as output by fit.nn. It should contain hidden.wts, output.wts, activation.function and form. Custom activation functions are not supported!
 #newdata: the data for which a prediction is desired.
 predict.AMORE = function( mod, newdata ){
@@ -102,126 +58,71 @@ predict.AMORE = function( mod, newdata ){
   return(preds)
 }
 
-#d: Dataset of interest. Should contain all training and test observations!
-#cvGroup: Specify the cross-validation group number for the training data (1 to # of cv groups, typically 10). -1=test data, 0=ignored.
-#indCol: The column of d containing the independent variable.
-#model: A string containing the model specification. data arguments will be ignored, and the function used is required to have a formula argument.
-#pred.cols: Some algorithms support multiple predictions (multiple averaged models, for example). pred.cols controls how many models should be estimated, and estimations are choosen in a meaningful way. Defaults to 1 (or 10 if gbm or fit.glmnet)
-#Currently supported functions:
-# fit.nn (defined above)
-# neuralnet
-# gbm
-# randomForest
-# glm
-# lm
-# rpart
-# glmnet
-# pcr
-# gam (from mgcv)
-# rbf (radial basis function neural network in RSNNS)
-cvModel = function(d, cvGroup, indCol, model="neuralnet(Y ~ X1 + X2 + X3 + X4 + X5, hidden=4, err.fct='sse')", pred.cols=1+9*grepl("(^fit.glmnet|^gbm)",model) ){
+#modelFunc: function for fitting the model.  Must take form, data or x,y as arguments, as well as args.
+#cvGroup: vector of length=nrow(d) or nrow(x).  1-k specify the k cross-validation groups, -1 specifies the test data
+#predFunc: function such that predFunc(fit, newdata) generates predictions for newdata.  newdata is of the same form as d or x.
+#d: data.frame to use for model.  Must be specified with form and x,y must be NULL.
+#form: model formula to use.  Must be specified with d and x,y must be NULL.
+#x: independent variables for the model (i.e. a matrix/data.frame).  Must be specified with y and data,form must be NULL.
+#y: dependent variable for the model (i.e. a vector).  Must be specified with x and data,form must be NULL.
+#args: additional arguments to pass to modelFunc, in the form of a named list.
+#pred.cols: How many prediction columns should be generated?  Usually this is 1, but in some special cases (i.e. gbm) multiple predictions are possible.  predFunc should be modified to return a matrix/data.frame with pred.cols number of columns.
+#saveMods: set to true if a list of all the fitted models should be returned as well as the fitted, cross-validated predictions
+#Change pred.cols to 10 for gbm and glmnet!!!
+cvModel = function(modelFunc, cvGroup, predFunc=predict, d=NULL, form=NULL, x=NULL, y=NULL
+        ,args=list(), pred.cols=1, saveMods=F ){
   #Data quality checks
-  if(indCol>ncol(d))
-    stop("indCol must be <= ncol(d)!")
-  if(length(cvGroup)!=nrow(d))
+  if(!is(modelFunc,"function"))
+    stop("func must be a function!")
+  if(is.null(form) & (!is.null(d) | is.null(x) | is.null(y)))
+    stop("Must specify form, d OR x, y")
+  if(is.null(x) & (!is.null(y) | is.null(form) | is.null(d)))
+    stop("Must specify form, d OR x, y")
+  if(!is.null(x) & !is.null(form))
+    stop("Must specify form, d OR x, y")
+  if(!is.null(x))
+    if(nrow(x)!=length(y))
+      stop("Number of rows of x must be the same as length of y!")
+  n = ifelse( is.null(form), length(y), nrow(d) )
+  if(length(cvGroup)!=n)
     stop("length(cvGroup)!=nrow(d).  Maybe cvGroup is a matrix?")  
   
-  ensem = data.frame( matrix(0, nrow=nrow(d), ncol=pred.cols ) )
+  ensem = data.frame( matrix(0, nrow=n, ncol=pred.cols ) )
   colnames(ensem) = paste0("V",1:ncol(ensem))
   #Set up the rownames of ensem to match d. This makes inserting in predicted values much easier later:
-  rownames(ensem) = 1:nrow(d)
-  rownames(d) = 1:nrow(d)
+  rownames(ensem) = 1:n
+  if(is.null(form)){
+    rownames(x) = 1:n
+  } else {
+    rownames(d) = 1:n
+  }
   
-  model = gsub("data=[A-Za-z0-9_.]*", "data=train", model )
-  model = gsub(" ","",model)
-  if(!grepl( "data=", model )) model = paste0(substr(model,1,nchar(model)-1),", data=train )")
-  
-  #Store the models, if desirable
+  #Store the models, if desirable.  Create a mods list in either case, so return method is always consistent.
   mods = list()
   
   #Set up the model formula and rename the columns of d appropriately:
   #Holdout each cv-group in turn:
   for( i in sort(unique(cvGroup[cvGroup>0])) ){
-    train = d[!cvGroup %in% c(-1,i),]
-    predict = d[cvGroup %in% c(-1,i),-indCol]
-    rownames(predict) = rownames(ensem)[cvGroup %in% c(-1,i)]
+    if(is.null(form)){
+      newArgs = c(list(y=y[!cvGroup %in% c(-1,i)]), args)
+      newArgs = c(list(x=x[!cvGroup %in% c(-1,i),]), newArgs)
+      predData = x[cvGroup %in% c(-1,i),]
+    } else {
+      newArgs = c(list(data=d[!cvGroup %in% c(-1,i),]), args)
+      newArgs = c(list(form=form), newArgs)
+      predData = d[cvGroup %in% c(-1,i),]      
+    }
+    rownames(predData) = rownames(ensem)[cvGroup %in% c(-1,i)]
     
     #Evaluate the model
-    fit = eval( parse( text=model) )
-    
-    #Predict based on the model used:
-    if( grepl("^neuralnet", model) ){
-      #neuralnet prediction requires a dataframe with only the used variables in it:
-      depCols = gsub( ",.*", "", gsub(".*~", "", model ) )
-      if(depCols=="." & length(depCols)==1)
-        predict.temp = predict
-      else
-      {
-        depCols = strsplit(depCols, "+", fixed=T)[[1]]
-        depCols = sapply(depCols, function(x){gsub(" ","",x)} )
-        predict.temp = predict[,colnames(predict) %in% depCols]
-      }
-      preds = compute(fit, predict.temp)$net.result
+    fit = do.call( modelFunc, args=newArgs )
+    preds = predFunc(fit, newdata=predData )
+    if(is(preds,"numeric")){
+      preds = matrix(preds,ncol=1)
+      rownames(preds) = rownames(predData)
     }
-    if( grepl("^fit.rbf", model) ){
-      #rbf prediction requires a dataframe with only the used variables in it:
-      depCols = gsub( ",.*", "", gsub(".*~", "", model ) )
-      if(depCols=="." & length(depCols)==1)
-        predict.temp = predict
-      else
-      {
-        depCols = strsplit(depCols, "+", fixed=T)[[1]]
-        depCols = sapply(depCols, function(x){gsub(" ","",x)} )
-        predict.temp = predict[,colnames(predict) %in% depCols]
-      }
-      preds = predict(fit, predict.temp)
-    }
-    if( grepl("^fit.nn", model) ){
-      preds = predict.nn(fit, newdata=predict)
+    if(saveMods)
       mods[[length(mods)+1]] = fit
-    }
-    if( grepl("^gbm", model) ){
-      if(pred.cols==1) warning("Only generating one prediction for a model that allows many!")
-      #Exponentially space out the trees for prediction, but round to nearest integer and remove duplicates:
-      tree.list = unique( round( exp(seq(0,log(fit$n.trees),length.out=pred.cols)) ) )
-      preds = data.frame(predict(fit, newdata=predict, n.trees=tree.list))
-      colnames(ensem) = paste0("gbm_trees",tree.list)
-      #Remove extra columns in ensem, if applicable
-      ensem = ensem[,1:ncol(preds)]
-    }
-    if( grepl("(^randomForest|^nnet)", model) ){
-      preds = data.frame(predict(fit, newdata=predict))
-      mods[[length(mods)+1]] = fit      
-    }
-    if( grepl("^([g]*lm)", model) ){
-      preds = data.frame(predict(fit, newdata=predict))
-      mods[[length(mods)+1]] = fit$coeff
-    }
-    if( grepl("^pcr", model) ){
-      #Prediction returns a 3-dimensional array (observations x prediction_type (always 1 here) x components). Extract and return all components
-      preds = apply(predict(fit, newdata=predict, type="response"), 3, identity)
-      if( ncol(preds)!=ncol(ensem) ){
-        warning(paste0("Overwriting pred.cols to return all components: ",pred.cols,"->",ncol(preds)))
-        if(ncol(ensem)<ncol(preds)) ensem = data.frame( matrix(0, nrow=nrow(d), ncol=ncol(preds)) )
-        if(ncol(ensem)>ncol(preds)) ensem = ensem[,1:ncol(preds)]
-      }
-      colnames(ensem) = colnames(preds)
-      preds = data.frame(preds)
-    }
-    if( grepl("^fit.glmnet", model) ){
-      if(pred.cols==1) warning("Only generating one prediction for a model that allows many!")
-      preds = data.frame(predict(fit, newx=as.matrix(predict)))
-      col.index = round(seq(1,ncol(preds),length.out=pred.cols))
-      preds = preds[,col.index]
-      colnames(ensem) = paste0("glmnet_lambda",round(fit$lambda[col.index],4))
-    }
-    if( grepl("^(gam|rpart)", model) ){
-      preds = data.frame(predict(fit, newdata=predict))
-    }
-    if( grepl("^naiveBayes", model) ){
-      preds = data.frame(predict(fit, newdata=predict, type="raw")[,2])
-    }
-    rownames(preds) = rownames(predict)
     
     #Insert the predicted values for the cv group into the ensem data.frame.
     pred.index = (1:nrow(ensem))[cvGroup==i]
